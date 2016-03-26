e915528e0095c80fd65b16ccbcff55299a2f10df
pashkin-sergey <pashkin_sergey@mail.ru>
2016-03-26 11:03:25 +0600

    input

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
+void input()
 {
+	float a,b,c;
+	printf("Wwedite koeficient a: \n");
+	scanf("%f",&a);
+	printf("Wwedite koeficient b: \n");
+	scanf("%f",&b);
+	printf("Wwedite koeficient c: \n");
+	scanf("%f",&c);
+	if (a==0)
+	{
+		printf("Ne kvadratnoe uravnenie");
+	}
 	return 0;
 }
