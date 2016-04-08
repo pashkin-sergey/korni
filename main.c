547d4b5b0f78d80209f9ec75952e2729201461f0
pashkin-sergey <pashkin_sergey@mail.ru>
2016-04-08 11:12:48 +0600

    Fixed code

--- main.c
+++ main.c
@@ -3,31 +3,29 @@
 
 void korni()
 {
-	float a,b,c,d;
+	float a, b, c, d;
 	printf("Wwedite koeficient a: \n");
-	scanf("%f",&a);
+	scanf("%f", &a);
 	printf("Wwedite koeficient b: \n");
-	scanf("%f",&b);
+	scanf("%f", &b);
 	printf("Wwedite koeficient c: \n");
-	scanf("%f",&c);
-	if (a==0)
+	scanf("%f", &c);
+	if (a == 0)
 	{
 		printf("Ne kvadratnoe uravnenie");
+		return -1;
+	}
+	d = b * b - 4 * a * c;
+		if (d < 0)
+	{
+		printf("korney net\n");
 	}
 	else 
 	{
-		d = b*b-4*a*c;
 		d = sqrt(d);
-		if (d < 0)
-		{
-			printf("korney net\n");
-		}
-		else 
-		{
-			x1 = (-b+d)/(2*a);
-			x2 = (-b-d)/(2*a);
-			printf("x1 = %f, x2 = %f\n",x1,x2);
-		}
+		x1 = (-b + d) / (2 * a);
+		x2 = (-b - d) / (2 * a);
+		printf("x1 = %f, x2 = %f\n", x1, x2);
 	}	
 	return 0;
 }
